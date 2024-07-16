4cd29b870b8ade04c14bf377a4a0d78a0ad1d1e8
Kopeikina Anna <kopkopkop47@gmail.com>
2024-07-16 22:50:31 +0300

    removed unnecessary junk

--- Assets/PixelRenderer/PerformanceMeasureForThesisStuff/NamedMonobehaviour.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-namespace Shaders.PerformanceMeasureForThesisStuff
-{
-    public class NamedMonobehaviour : MonoBehaviour
-    {
-        public string Name;
-    }
-}
--- Assets/Scripts/Drops/EntityDrop.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-namespace Drops
-{
-    public class EntityDrop : MonoBehaviour
-    {
-        
-    }
-}
